e2c10f5358cce67cdb811f5b0d5786c6bc40ec2f
Damir Myrzabaev <damir_60@live.com>
2024-04-03 16:01:16 +0500

    Create main.go

---
+++ 03_lesson/Task_3.4/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+const (
+	myConst1 = 10
+	myConst2 = 20
+	myConst3 = 30
+	myConst4 = 40
+	myConst5 = 50
+)
+
+func main() {
+	fmt.Printf("Результат: %d, %d, %d, %d, %d", myConst1, myConst2, myConst3, myConst4, myConst5)
+}
