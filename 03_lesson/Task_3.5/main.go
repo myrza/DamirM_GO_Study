752b5285ca3165ffbf7ce45562e385a01671c2e3
Damir Myrzabaev <damir_60@live.com>
2024-04-03 16:05:05 +0500

    Task3.5

---
+++ 03_lesson/Task_3.5/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	const (
+		myConst1 = 101
+		myConst2 = 201
+		myConst3 = 301
+		myConst4 = 401
+		myConst5 = 501
+	)
+	fmt.Printf("Результат: %d, %d, %d, %d, %d", myConst1, myConst2, myConst3, myConst4, myConst5)
+}
