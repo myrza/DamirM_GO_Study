37ab78f78f94f746a487ed0354998907c187f9ef
Damir Myrzabaev <damir_60@live.com>
2024-06-07 17:46:36 +0500

    17.3

--- 17_lesson/Task_17.3/main.go
+++ 17_lesson/Task_17.3/main.go
@@ -14,6 +14,19 @@ import (
 )
 
 func main() {
+
+	LOG_FILE := "myapp_log"
+
+	f, err := os.OpenFile(LOG_FILE, os.O_APPEND|os.O_RDWR|os.O_CREATE, 0644)
+
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer f.Close()
+
+	log.SetOutput(f)
+	log.SetFlags(log.Lshortfile | log.LstdFlags)
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/hello", hello)
 
@@ -28,6 +41,7 @@ func main() {
 	if err := httpServer.ListenAndServe(); err != nil {
 		log.Fatalln(fmt.Errorf("Неудалось запустить сервер: %w", err))
 	}
+
 }
 
 func hello(res http.ResponseWriter, req *http.Request) {
@@ -35,6 +49,8 @@ func hello(res http.ResponseWriter, req *http.Request) {
 	//log.Println(res.Header())
 	msg := "Hello, Go!"
 	log.Println("resp:", msg)
+	log.Println("resp:", msg)
+
 	fmt.Fprint(res, msg)
 }
 
