6b08cc5ae2e7d5736f6dab234f1041730734ed0f
Damir Myrzabaev <damir_60@live.com>
2024-06-07 15:01:32 +0500

    1

--- 13_lesson/Task_13.5/main.go
+++ 13_lesson/Task_13.5/main.go
@@ -1,75 +1,28 @@
 /*
-В рамках задачи будем работать с картотекой известного врача. Нужно будет написать модуль с несколькими версиями: v1.0.0, v1.1.0, v2.0.0, v2.1.0. Модуль должен прочитать файл со следующим содержимым:
-
-{"name":"Ёжик","age":10,"email":"ezh@mail.ru"}
-{"name":"Зайчик","age":2,"email":"zayac@mail.ru"}
-{"name":"Лисичка","age":3,"email":"alice@mail.ru"}
-
-v1.0.0 должна создавать файл с содержимым:
-v1.1.0 должна сортировать данные по полю age по
-возрастанию:
-v2.0.0 должна создавать файл с содержимым:
-<?xml version="1.0" encoding="UTF-8"?>
-Модуль должен содержать функцию Do, которая принимает два
-строковых параметра: путь файла откуда прочитать данные, путь
-файла, в который записать данные в требуемом формате; и
-возвращать ошибку. Пример использования модуля:
-package main
-import (
-format ...
-)
-func main() {
-err := format.Do("patients", «result")
-if err != nil {
-…
-}
-}
-Должна быть возможность подключить любую из версий: v1.0.0, v1.1.0,
-v2.0.0, v2.1.0.
-
+	go get
+	github.com/myrza/patients@v1.0.0 из json в json без сортировки
+	github.com/myrza/patients@v1.1.0 из json в json c сортировкой по годам
+	github.com/myrza/patients@v2.0.0 из json в xml без сортировки
+	github.com/myrza/patients@v2.1.0 из json в xml c сортировкой по годам
 }
 */
 
 package main
 
 import (
-	"encoding/xml"
 	"fmt"
+
+	v "github.com/myrza/patients"
 )
 
-type Contractor interface {
-}
-type contract struct {
-	Number   int    `xml:"number"`
-	Landlord string `xml:"landlord"`
-	Tenat    string `xml:"tenat"`
-}
-
-type contracts struct {
-	List []contract `xml:"contract"`
-}
-
 func main() {
-	c1 := contract{
-		Number:   1,
-		Landlord: "Остап Бендер",
-		Tenat:    "Шура Балаганов",
-	}
-	c2 := contract{
-		Number:   2,
-		Landlord: "Остап Бендер",
-		Tenat:    "Шура Балаганов",
-	}
-	d := contracts{}
-	d.List = append(d.List, c1)
-	d.List = append(d.List, c2)
-
-	res, err := xml.Marshal(d)
-
-	if err != nil {
-		fmt.Println("Error:", err)
+	src := "patients.txt"
+	tgt := "result"
+	err := v.Do(src, tgt)
+	if err == nil {
+		fmt.Println("Файл записан успешно")
+	} else {
+		fmt.Println(err)
 	}
 
-	fmt.Println(xml.Header, string(res))
-	fmt.Println()
 }
---
+++ 13_lesson/Task_13.5_old/main.go
@@ -0,0 +1,75 @@
+/*
+В рамках задачи будем работать с картотекой известного врача. Нужно будет написать модуль с несколькими версиями: v1.0.0, v1.1.0, v2.0.0, v2.1.0. Модуль должен прочитать файл со следующим содержимым:
+
+{"name":"Ёжик","age":10,"email":"ezh@mail.ru"}
+{"name":"Зайчик","age":2,"email":"zayac@mail.ru"}
+{"name":"Лисичка","age":3,"email":"alice@mail.ru"}
+
+v1.0.0 должна создавать файл с содержимым:
+v1.1.0 должна сортировать данные по полю age по
+возрастанию:
+v2.0.0 должна создавать файл с содержимым:
+<?xml version="1.0" encoding="UTF-8"?>
+Модуль должен содержать функцию Do, которая принимает два
+строковых параметра: путь файла откуда прочитать данные, путь
+файла, в который записать данные в требуемом формате; и
+возвращать ошибку. Пример использования модуля:
+package main
+import (
+format ...
+)
+func main() {
+err := format.Do("patients", «result")
+if err != nil {
+…
+}
+}
+Должна быть возможность подключить любую из версий: v1.0.0, v1.1.0,
+v2.0.0, v2.1.0.
+
+}
+*/
+
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+)
+
+type Contractor interface {
+}
+type contract struct {
+	Number   int    `xml:"number"`
+	Landlord string `xml:"landlord"`
+	Tenat    string `xml:"tenat"`
+}
+
+type contracts struct {
+	List []contract `xml:"contract"`
+}
+
+func main() {
+	c1 := contract{
+		Number:   1,
+		Landlord: "Остап Бендер",
+		Tenat:    "Шура Балаганов",
+	}
+	c2 := contract{
+		Number:   2,
+		Landlord: "Остап Бендер",
+		Tenat:    "Шура Балаганов",
+	}
+	d := contracts{}
+	d.List = append(d.List, c1)
+	d.List = append(d.List, c2)
+
+	res, err := xml.Marshal(d)
+
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+
+	fmt.Println(xml.Header, string(res))
+	fmt.Println()
+}
