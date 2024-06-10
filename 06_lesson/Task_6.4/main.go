4da9aa65aaa04306abd4e4b297c7233ba391ac1f
Damir Myrzabaev <damir_60@live.com>
2024-06-10 21:05:56 +0500

    6.4

--- 06_lesson/Task_6.4/main.go
+++ 06_lesson/Task_6.4/main.go
@@ -22,19 +22,24 @@ import (
 	"fmt"
 )
 
+type contact struct {
+	Addss, Phone string
+}
 type user struct {
-	ID                 int
-	Name, Addss, Phone string
+	ID   int
+	Name string
+	contact
 }
 
 type employee struct {
-	ID int
-	user
+	ID   int
+	Name string
+	contact
 }
 
 func main() {
-	u := user{ID: 1, Name: "Вася", Addss: "Москва, 3 улица Строителей 2", Phone: "911-111-111"}
-	e := employee{ID: 1, user: user{ID: 1, Name: "Пупкин", Addss: "Москва, Марьина роща 2", Phone: "777-222-222"}}
+	u := user{ID: 1, Name: "Вася", contact: contact{"Москва, 3 улица Строителей 2;", "911-111-111"}}
+	e := employee{ID: 1, Name: "Никанор Босой", contact: contact{"Москва, Марьина роща 2;", "777-222-222"}}
 
-	fmt.Println("Пользователь: ", u.Name, u.Addss, u.Phone, " \nСотрудник: ", e.Name, e.Addss, e.Phone)
+	fmt.Println("Пользователь: ", u.Addss, u.Phone, " \nСотрудник: ", e.Addss, e.Phone)
 }
